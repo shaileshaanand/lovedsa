30e810b097f187ea3bce0c68fe66a1982819805a
Shailesh Aanand <anaandshailu@gmail.com>
2022-01-01 16:33:14 +0530

    cleanup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      Hi Nav
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Stats.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Stats() {
+  return (
+    <div>
+      <Navbar />
+      <div>Hi</div>
+    </div>
+  );
+}
+
+export default Stats;
